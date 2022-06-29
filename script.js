402c81d7f6676e013481466968010011a9ac4a57
SuperAtraction <91375731+SuperAtraction@users.noreply.github.com>
2022-06-29 14:07:51 +0200

    WebEdit#2 => 29/06/2022

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
+var IsLoad = 0;
 $(document).ready(function(){
-    var str = window.location.href;
-    var url = new URL(str);
-    var search_params = new URLSearchParams(url.search); 
-    if(search_params.has('code')) {
-        var code = search_params.get('code');
-        document.write(code);
-    }else {
-        location.href="../informatique/Web/Utiliser-WebEdit.html";
+    if(IsLoad==0){
+        var str = window.location.href;
+        var url = new URL(str);
+        var search_params = new URLSearchParams(url.search); 
+        if(search_params.has('code')) {
+            var code = search_params.get('code');
+            document.write(code);
+        }else {
+            location.href="../informatique/Web/Utiliser-WebEdit.html";
+        }
     }
 });
