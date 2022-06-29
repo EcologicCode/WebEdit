702130327dbb47c66341c3d12ff6128450698b90
SuperAtraction <91375731+SuperAtraction@users.noreply.github.com>
2022-06-29 15:27:04 +0200

    WebEdit#6 => 29/06/2022

--- script.js
+++ script.js
@@ -7,8 +7,9 @@ $(document).ready(function(){
         if(search_params.has('code')) {
             var code = search_params.get('code');
             if(!search_params.has('JavaScript')){
-            $("#docu").html("<div style=\"background-color:rgb(0, 0, 0);color:white\"><a href=\""+str+"&JavaScript=true\"Javascript ?</div><br>"+code);
+            $("#docu").html("<div style=\"background-color:rgb(0, 0, 0);color:rgb(255, 255, 255);\"><a href=\""+str+"&JavaScript=true\"Javascript ?</a></div><br>"+code);
             }else {
+                $("#docu").html(code);
                 if(search_params.has('script')){
                     var Script = search_params.get('script');
                     $.getScript(Script);
